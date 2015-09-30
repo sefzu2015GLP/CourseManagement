cb36861896ef12847d9030edd4eaf71ccfca5d0a
heronsbill <981141175@qq.com>
2015-09-30 19:07:25 +0800

    the UI of our project

---
+++ MyApplication/app/src/main/java/com/example/pgr/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+
+package com.example .pgr.myapplication ;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity  {
+    private Button button;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+
+        editText = (EditText) findViewById(R.id.edit_text1 );
+       // button.setOnClickListener(this );
+    }
+
+  //  @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText,
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
